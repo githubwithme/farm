94dbe22d1fb5e72209844b21e478e6b369ca00bd
scholat <qy956935952@163.com>
2016-05-28 23:03:16 +0800

    红点处理

--- app/src/main/java/com/farm/app/AppContext.java
+++ app/src/main/java/com/farm/app/AppContext.java
@@ -51,7 +51,6 @@ public class AppContext extends Application
     public final static String BROADCAST_SHOWDIALOG = "BROADCAST_SHOWDIALOG";
     public final static String BROADCAST_PG_UPEVENT = "PG_UPEVENT";
     public final static String BROADCAST_PG_REFASH = "PG_REASH";
-    public final static String BROADCAST_PG_UPEVENT = "PG_UPEVENT";
     public final static String TAG_NCZ_CMD = "TAG_NCZ_CMD";
     public final static String BROADCAST_PG_DATA = "PG_DATA";
     public final static String BROADCAST_Record = "EVENT_RECORD";
