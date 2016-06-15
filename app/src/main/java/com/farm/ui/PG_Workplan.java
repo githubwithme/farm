dd0375495b8eddfcf2c336f34afebc38d0528f33
scholat <qy956935952@163.com>
2016-06-15 09:40:22 +0800

    无

---
+++ app/src/main/java/com/farm/ui/PG_Workplan.java
@@ -0,0 +1,21 @@
+package com.farm.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.farm.R;
+
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Created by hasee on 2016/6/14.
+ */
+@EActivity(R.layout.pg_workplan)
+public class PG_Workplan extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
